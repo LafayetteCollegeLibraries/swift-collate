5efe9fb0b74aaf5c9e57163c442d6cfd99ceb9e3
jrgriffiniii <griffinj@lafayette.edu>
2015-02-17 16:24:06 -0500

    SPP-160 #time 2h Implementing the edit distance calculations for the TEI parse trees

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -3,6 +3,7 @@ import sys
 import pytest
 
 from lxml import etree
+import nltk
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from tokenizer import Tokenizer
@@ -30,6 +31,42 @@ class TestTokenizer:
 
         return elem
 
+    @pytest.fixture
+    def tei_stanza_a(self):
+
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/test_tei_a.xml')) as f:
+
+            data = f.read()
+            doc = etree.fromstring(data)
+            elems = doc.xpath('//tei:lg', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+            elem = elems.pop()
+
+        return elem
+
+    @pytest.fixture
+    def tei_stanza_b(self):
+
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/test_tei_b.xml')) as f:
+
+            data = f.read()
+            doc = etree.fromstring(data)
+            elems = doc.xpath('//tei:lg', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+            elem = elems.pop()
+
+        return elem
+
+    @pytest.fixture
+    def tei_stanza_c(self):
+
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/test_tei_c.xml')) as f:
+
+            data = f.read()
+            doc = etree.fromstring(data)
+            elems = doc.xpath('//tei:lg', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+            elem = elems.pop()
+
+        return elem
+
     def test_init(self):
 
         pass
@@ -39,12 +76,24 @@ class TestTokenizer:
         tokenizer = Tokenizer()
         tree = Tokenizer.parse(tei_stanza)
 
-        assert tree.has_edge('l_@n="1"', 'Piping')
-        assert tree.has_edge('l_@n="1"', 'down')
-        assert tree.has_edge('l_@n="1"', 'the')
-        assert tree.has_edge('l_@n="1"', 'valleys')
-        assert tree.has_edge('l_@n="1"', 'wild')
+        assert tree.has_edge('<l n="1" />', 'Piping down the valleys wild, ')
 
-        assert tree.has_edge('l_@n="2"', 'songs')
-        assert tree.has_edge('l_@n="3"', 'cloud')
-        assert tree.has_edge('l_@n="4"', 'laughing')
+        assert tree.has_edge('<l n="2" />', 'Piping songs of pleasant glee, ')
+        assert tree.has_edge('<l n="3" />', 'On a cloud I saw a child, ')
+        assert tree.has_edge('<l n="4" />', 'And he laughing said to me: ')
+
+    def test_text_diff(self, tei_stanza_a, tei_stanza_b):
+
+        tokenizer = Tokenizer()
+
+        diff_tree = Tokenizer.diff(tei_stanza_a, tei_stanza_b)
+        
+        assert diff_tree['<l n="3" />']['On a cloud I saw a child, ']['distance'] == nltk.metrics.distance.edit_distance('On a cloud I saw a child, ', 'On cloud I saw child ')
+
+    def test_struct_diff(self, tei_stanza_a, tei_stanza_c):
+
+        tokenizer = Tokenizer()
+        
+        diff_tree = Tokenizer.diff(tei_stanza_a, tei_stanza_c)
+
+        assert diff_tree['<l n="3" />']['On a cloud I saw a child, ']['distance'] == nltk.metrics.distance.edit_distance('On a cloud I saw a child, ', 'On a cloud I  saw  a child, ')
--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,9 @@
 
 from nltk.tokenize.punkt import PunktWordTokenizer
 import networkx as nx
+import re
+import nltk
+import string
 
 class TextToken:
 
@@ -17,18 +20,19 @@ class ElementToken:
             name = doc.xpath('local-name()')
             attrib = doc.attrib
             children = list(doc)
-            text = doc.text
+            text = string.join(list(doc.itertext())) if doc.text is not None else ''
 
         self.name = name
         self.attrib = attrib
 
         # Generate a string consisting of the element name and concatenated attributes (for comparison using the edit distance)
-        # Note: the athtributes *must* be order by some arbitrary feature
+        # Note: the attributes *must* be order by some arbitrary feature
 
-        self.value = self.name
+        self.value = '<' + self.name
         for attrib_name, attrib_value in self.attrib.iteritems():
             
-            self.value += '_@' + attrib_name + '="' + attrib_value + '"'
+            self.value += ' ' + attrib_name + '="' + attrib_value + '"'
+        self.value += ' />'
 
         self.children = children
 
@@ -62,24 +66,17 @@ class Tokenizer:
         # /     \
         # n-gram n-gram
 
-        # Default to Punkt
-        tokenizer = PunktWordTokenizer()
+        # text_tokens = tokenizer.tokenize( token_tree_root.text )
+        text_tokens = [ token_tree_root.text ]
 
-        print 'trace3'
-        print node
-        print node.text
-        print token_tree_root.name
-        print token_tree_root.text
+        init_dist = 0
 
-        # text_tokens = map( tokenizer.tokenize, token_tree_root.text )
-        text_tokens = tokenizer.tokenize( token_tree_root.text )
+        # Introduce the penalty for stylistic or transcription elements
+        # @todo This also requires some problematic restructuring of footnote and rendering elements (as these are *not* leaves)
 
-
-        # if len(text_tokens) > 0:
-        if True:
-
-            # text_token_edges = map(lambda token: (token_tree_root, TextToken(token)), text_tokens )
-            text_token_edges = map(lambda token: (token_tree_root.value, token), text_tokens )
+        # text_token_edges = map(lambda token: (token_tree_root, TextToken(token)), text_tokens )
+        text_token_edges = map(lambda token: (token_tree_root.value, token, { 'distance': init_dist }), text_tokens )
+        # text_token_edges = map(lambda token: (token_tree_root, token), text_tokens )
 
         token_tree.add_edges_from(text_token_edges)
 
@@ -97,3 +94,66 @@ class Tokenizer:
 
             token_tree = nx.compose(token_tree, sub_tree)
         return token_tree
+
+    @staticmethod
+    def diff(node_u, node_v):
+
+        tree_u = Tokenizer.parse(node_u)
+        tree_v = Tokenizer.parse(node_v)
+
+        diff_tree = nx.Graph()
+
+        # Assess the difference in tree structure
+        # diff_tree = nx.difference(tree_u, tree_v)
+
+        # Calculate the edit distance for each identical node between the trees
+
+        # Retrieve the common edges
+        # intersect_tree = nx.intersection(tree_u, tree_v)
+
+        # Iterate through each edge
+        # for edge in intersect_tree.edges(data=True):
+        for u, v, data in tree_u.edges(data=True):
+
+            # Only perform the edit distance for text nodes
+            # edit_dist = nltk.metrics.distance(tree_u[u], tree_v[u])
+            # (u, u, edit_dist)
+
+            # text_nodes = filter( lambda e: e.__class__.__name__ != 'ElementToken', [u,v] )
+            text_nodes = filter( lambda e: not re.match(r'^<', e), [u,v] )
+
+            if len(text_nodes) > 1:
+
+                raise Exception("Text nodes can not be linked to text nodes", text_nodes)
+            elif len(text_nodes) == 0:
+
+                # Comparing elements
+                assert False
+            else:
+                
+                text_node_u = string.join(text_nodes)
+                elem_node_u = v if u == text_node_u else u
+                nodes_u_dist = data['distance']
+
+                # Retrieve the same text node from the second tree
+                text_nodes_v = tree_v[elem_node_u].keys()
+                
+                text_node_v = string.join(text_nodes_v)
+                nodes_v_dist = tree_v[elem_node_u][text_node_v]['distance']
+
+                # Default to Punkt
+                # tokenizer = PunktWordTokenizer()
+
+                # Just add the edit distance
+                edit_dist = nodes_u_dist + nodes_v_dist + nltk.metrics.distance.edit_distance(text_node_u, text_node_v)
+                
+                diff_tree.add_edge(elem_node_u, text_node_u, distance=edit_dist)
+                pass
+
+            pass
+
+        # Generate the edit distance
+        
+        # Append the edge to the diff_tree, with the edit distance as the attribute
+
+        return diff_tree
