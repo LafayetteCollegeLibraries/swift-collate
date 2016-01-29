ca422e66bf613e01ef2a993804661354296ff9ce
jrgriffiniii <griffinj@lafayette.edu>
2016-01-29 17:19:21 -0500

    SPP-31 #time 3h 14m Integrating the python-daemon Module and structuring the system service for the application; Extending the integration for the service and appl. configuration settings with ConfigParser

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
+#!/usr/bin/env python
 
 import os.path
 import string
+import re
+import subprocess
+import os
+import time
+import importlib
+import ConfigParser
+import fnmatch
+import pickle
+import logging
+import signal
+
+import networkx as nx
+from lxml import etree
 
 import tornado.ioloop
 import tornado.web
@@ -8,41 +22,38 @@ import tornado.escape
 import tornado.httpserver
 from tornado.concurrent import run_on_executor
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
-
 from tornado import gen, queues
+from tornado.options import define, options, parse_command_line
 
-import networkx as nx
-import re
+from pymongo import MongoClient
+from bson.binary import Binary
 
-# from SwiftDiff.collation import Collation
-# from SwiftDiff import TextToken, Line, Text, DifferenceText, Collation
-# from SwiftDiff.tokenizer import Tokenizer
 from SwiftDiff import TextToken
 from SwiftDiff.text import Line, Text
 from SwiftDiff.collate import DifferenceText, Collation
 from SwiftDiff.tokenize import Tokenizer, SwiftSentenceTokenizer
 
-from tornado.options import define, options, parse_command_line
+MAX_WAIT_SECONDS_BEFORE_SHUTDOWN = 3
 
-MAX_WORKERS=18
+# Retrieve the configuration settings
+config = ConfigParser.RawConfigParser()
+config.read( os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config', 'swift_collate.cfg')  )
 
-define("port", default=8888, help="run on the given port", type=int)
+# Work-around for multiprocessing within Tornado
+max_workers = config.getint('server', 'max_workers')
+pool = ProcessPoolExecutor(max_workers=max_workers)
+
+# Retrieve the cache configuration
+host = config.get('MongoDB', 'host')
+port = config.getint('MongoDB', 'port')
+client = MongoClient(host, port)
+db_name = config.get('MongoDB', 'db_name')
+
+# Set the global variables for the server
+define("port", default=8889, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode")
-define("processes", default=MAX_WORKERS, help="concurrency")
+define("processes", default=max_workers, help="concurrency")
 define("cache", default=True, help="caching", type=bool)
-
-import subprocess
-import os
-import time
-from lxml import etree
-
-import importlib
-
-from pymongo import MongoClient
-from bson.binary import Binary
-import ConfigParser
-import fnmatch
-import pickle
 
 class FootnotesModule(tornado.web.UIModule):
 
@@ -125,20 +136,18 @@ class TokenModule(tornado.web.UIModule):
             
         return self.render_string("token.html", token=token_value, classes=classes)
 
-# Work-around for multiprocessing within Tornado
-pool = ProcessPoolExecutor(max_workers=MAX_WORKERS)
-
-# Retrieve the database
-config = ConfigParser.RawConfigParser()
-config.read( os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config', 'swift_collate.cfg')  )
-    
-host = config.get('MongoDB', 'host')
-port = config.getint('MongoDB', 'port')
-client = MongoClient(host, port)
-
 def cache(collection_name, key, value=None):
+    """Retrieves, creates, or updates a cached data structure into the cache store
 
-    db = client['swift_collate']
+    :param collection_name: The name of the MongoDB collection
+    :type collection_name: str
+
+    :param key: The key for the Object
+
+    :param value: The data structure for insert and update operations
+    """
+
+    db = client[db_name]
     collection = db[collection_name]
 
     if value is None:
@@ -350,7 +359,7 @@ class CollateHandler(tornado.web.RequestHandler):
                 witness_values = { 'node': node, 'id': witness_id }
                 variant_texts.append( witness_values )
             else:
-                print "Failed to parse the XML for %s" % witness_id
+                logging.warn("Failed to parse the XML for the transcript %s", witness_id)
 
         # Retrieve the base Text
         base_text = Text(base_doc, base_id, SwiftSentenceTokenizer)
@@ -510,6 +519,29 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+# Please see https://gist.github.com/mywaiting/4643396
+def sig_handler(sig, frame):
+    logging.warning('Caught signal: %s', sig)
+    tornado.ioloop.IOLoop.instance().add_callback(shutdown)
+
+def shutdown():
+    logging.info('Stopping http server')
+    server.stop()
+
+    logging.info('Will shutdown in %s seconds ...', MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+    io_loop = tornado.ioloop.IOLoop.instance()
+
+    deadline = time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN
+
+    def stop_loop():
+        now = time.time()
+        if now < deadline and (io_loop._callbacks or io_loop._timeouts):
+            io_loop.add_timeout(now + 1, stop_loop)
+        else:
+            io_loop.stop()
+            logging.info('Shutdown')
+    stop_loop()
+
 def main():
 
     tei_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'xml')
@@ -536,11 +568,17 @@ def main():
         debug=options.debug,
         ui_modules={ "Token": TokenModule, "Line": LineModule, "Footnotes": FootnotesModule },
         )
-    # app.listen(options.port)
+
+    # https://gist.github.com/mywaiting/4643396
+    global server
     server = tornado.httpserver.HTTPServer(app)
     server.bind(options.port)
-    server.start(MAX_WORKERS)
-    CollateHandler.executor = ProcessPoolExecutor(max_workers=MAX_WORKERS)
+    server.start(max_workers)
+
+    signal.signal(signal.SIGTERM, sig_handler)
+    signal.signal(signal.SIGINT, sig_handler)
+
+    CollateHandler.executor = ProcessPoolExecutor(max_workers=max_workers)
     ioloop = tornado.ioloop.IOLoop.current().start()
     # ioloop = tornado.ioloop.IOLoop.instance()
     # executor = Executor()
@@ -548,5 +586,4 @@ def main():
     # ioloop.start(0)
 
 if __name__ == "__main__":
-
     main()
