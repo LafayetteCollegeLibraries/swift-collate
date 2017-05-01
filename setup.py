39dfa3ba3b5f3ae18c0540fe7dbeec3419ee521a
James R. Griffin III <jrgriffiniii@gmail.com>
2017-05-01 16:57:29 -0400

    Resolves appdirs dependency issues and for the version Module (#17)
    * Explicitly listing additional Package dependencies in setup.py
    
    * Explicitly passing the version number in setup.py
    
    * Setting appdirs to release 1.4.2 (in response to https://github.com/ActiveState/appdirs/issues/89)
    
    * Attempting to remove the appdirs dependency outright

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-from swift_collate.version import __version__
 
 setup(
     name='swift_collate',
-    version=__version__,
+    version='0.0.1',
     description='A Python API for collating TEI-XML Documents encoded for the Swift Poems Project',
     author='James R. Griffin III',
     author_email='griffinj@lafayette.edu',
@@ -13,9 +12,17 @@ setup(
     license='GPLv3+',
     packages=find_packages(),
     install_requires=[
-        'nltk>=3.2.2',
-        'lxml>=3.7.2',
-        'collatex==2.0.0rc9'
+        'nltk==3.2.2',
+        'lxml==3.7.2',
+        'collatex==2.0.0rc9',
+        'decorator==4.0.11',
+        'networkx==1.11',
+        'packaging==16.8',
+        'prettytable==0.7.2',
+        'py==1.4.32',
+        'pyparsing==2.1.10',
+        'pytest==3.0.6',
+        'six==1.10.0'
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'])
