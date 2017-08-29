312e358e7d8373e391953350c3aaf237e3690567
adam malantonio <adam.malantonio@gmail.com>
2017-08-29 11:07:54 -0400

    change package name to swift_collate
    this is how it's required from within swift-edition

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='SwiftDiff',
-    version='0.1',
+    name='swift_collate',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     author='jrgriffiniii',
     author_email='griffinj@lafayette.edu',
-    url='https://github.com/LafayetteCollegeLibraries/swift-diff',
+    url='https://github.com/LafayetteCollegeLibraries/swift-collate',
     description='Textual analysis web service for the [Swift Poems Project](http://swift.lafayette.edu)',
     keywords=[],
     zip_safe=False
